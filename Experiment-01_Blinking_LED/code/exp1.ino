1fffbfac2221074041bcc5d8d36780f4e6812bb1
DeeprajNaik28 <deeprajnaik0028@gmail.com>
2026-07-22 19:59:15 +0530

    exp-1 completed

---
+++ Experiment-01_Blinking_LED/code/exp1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  
+  digitalWrite(12, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(12, HIGH);
+  delay(1000);                      
+}
